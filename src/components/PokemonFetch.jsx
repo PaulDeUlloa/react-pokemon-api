e654b450bcf62971c7c3e24092416c99629f13d4
Paul De Ulloa <pauld143@gmail.com>
2023-09-05 21:13:05 -0700

    restructuring occured of all files

--- src/components/PokemonFetch.jsx
+++ src/components/PokemonFetch.jsx
@@ -1,47 +1,4 @@
-import React from "react";
-import { useState } from "react";
-import axios from "axios";
-
-import PokemonEntry from "./PokemonEntry";
-import PokemonSearch from "./PokemonSearch";
-
-
-const PokemonFetch = () => {
-    const [pokemon, setPokemon] = useState();
-
-    const getPokemonFetch = () => {
-        fetch(`https://pokeapi.co/api/v2/pokemon?limit=807&offset=0`)
-            .then((res) => {
-                // If we return here, we'll get our response converted to JSON.
-                return res.json(); // res.json() is another promise, so we'll need a .then() to pause until it finishes!
-            })
-            .then((jsonRes) => {
-
-                // Now we can pick up the return from the previous function as "jsonRes" and now we have the finished JSON object.
-                console.log(jsonRes);
-                setPokemon(jsonRes);
-            })
-            .catch((err) => console.log(err));
-    };
-    const getPokemonAxios = () => {
-        //! Axios allows us to skip converting the object we fetch into JSON.
-        axios
-            .get(`https://pokeapi.co/api/v2/pokemon/snorlax`)
-            .then((res) => {
-                setPokemon(res.data);
-            })
-            .catch((err) => console.log(err));
-    };
-    const searchPokemonAxios = (pokeName) => {
-        //! Axios allows us to skip converting the object we fetch into JSON.
-        axios
-            .get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
-            .then((res) => {
-                setPokemon(res.data);
-            })
-            .catch((err) => console.log(err));
-    };
-
+const PokemonFetch = (props) =>{
     return (
         <fieldset>
             <legend>PokemonFetch.jsx</legend>
@@ -49,21 +6,7 @@ const PokemonFetch = () => {
                 Let's fetch some Pokemon Data!
             </p>
 
-            <button onClick={getPokemonFetch}>Fetch Pokemon Data</button>
-            <button onClick={getPokemonAxios}>Axios Pokemon Data</button>
-
-            <PokemonSearch searchPokemonAxios={searchPokemonAxios} />
-
-            <div>
-                {pokemon ? (
-                    <PokemonEntry
-                        pokeName={pokemon.name}
-                        pokeImg={pokemon.sprites.front_default}
-                    />
-                ) : (
-                    <p>Click button to start!</p>
-                )}
-            </div>
+            <button onClick={() => props.fetchPokemon()}>Fetch Pokemon</button>
         </fieldset>
     );
 
