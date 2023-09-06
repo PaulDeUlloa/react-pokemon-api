114f3447a200dbb3524530a8c0299fb8b4482fe5
Paul De Ulloa <pauld143@gmail.com>
2023-09-05 21:21:39 -0700

    function app added to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
 import PokemonFetch from "./components/PokemonFetch";
+import PokemonName from "./components/PokemonName";
+import { useState } from 'react';
+
+function App(){
+  const [pokemonName, setPokemonName] = useState([]);
+
+  const fetchPokemon = () => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/charizard`)
+    .then((res) => {
+
+        return res.json(); 
+    })
+    .then((jsonRes) => {
+        
+        console.log(jsonRes);
+        setPokemonName(jsonRes);
+    })
+    .catch((err) => console.log(err));
+  }
+}
 
 function App() {
   return (
     <div className="container mt-3 d-flex flex-column align-items-center">
-      <PokemonFetch />
+      <PokemonFetch fetchPokemon={fetchPokemon} />
+      <PokemonName pokemonName={pokemonName} />
     </div>
   );
 }
