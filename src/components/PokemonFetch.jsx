b42375fa817798459ca68a20f185a3d25a7daee0
Paul De Ulloa <pauld143@gmail.com>
2024-03-24 23:38:08 -0700

    css styling for PokemonFetch page

--- src/components/PokemonFetch.jsx
+++ src/components/PokemonFetch.jsx
@@ -1,15 +1,14 @@
 const PokemonFetch = (props) => {
-    return (
-        <fieldset>
-            <legend>PokemonFetch.jsx</legend>
-            <p>
-                Let's fetch some Pokemon Data!
-            </p>
+  return (
+    <fieldset id="pokemonFetchTextStyling">
+      <legend>PokemonFetch.jsx</legend>
+      <p>Let's fetch some Pokemon Data!</p>
 
-            <button onClick={() => props.fetchPokemon()}>Fetch Pokemon</button>
-        </fieldset>
-    );
-
+      <button class="btn btn-info" onClick={() => props.fetchPokemon()}>
+        Fetch Pokemon
+      </button>
+    </fieldset>
+  );
 };
 
 export default PokemonFetch;
