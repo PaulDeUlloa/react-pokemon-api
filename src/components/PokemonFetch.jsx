1cd6d17dfccd1006469256edcbef1723ae60fd59
Paul De Ulloa <pauld143@gmail.com>
2024-03-24 23:40:55 -0700

    centering adjustment

--- src/components/PokemonFetch.jsx
+++ src/components/PokemonFetch.jsx
@@ -1,7 +1,7 @@
 const PokemonFetch = (props) => {
   return (
     <fieldset id="pokemonFetchTextStyling">
-      <legend>PokemonFetch.jsx</legend>
+      <legend id="pokemonFetchTitle">PokemonFetch</legend>
       <p>Let's fetch some Pokemon Data!</p>
 
       <button class="btn btn-info" onClick={() => props.fetchPokemon()}>
